00d2fec10dc8f369e02d96d4bbb6c726b6914279
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-02-20 14:35:23 -0300

    feat: create user list class

---
+++ src/models/UserList.js
@@ -0,0 +1,33 @@
+class UserList {
+    constructor() {
+        this.users = [];
+    }
+
+    addUser(User) {
+        this.users.push(User);
+    }
+
+    getAllUsers() {
+        return this.users;
+    }
+
+    getUserById(id) {
+        const user = this.users.find(user => user.id == id);
+        if(!user){
+            throw new Error("usuário não encontrado :(");
+        }
+        return user;
+    }
+
+    updateUser(id, updateData) {
+        const user = this.getUserById(id);
+        Object.assign(user, updateData);
+        return user;
+    }
+
+    deleteUser(id) {
+        this.users = this.users.filter(user => user.id != id);
+    }
+}
+
+module.exports = UserList;
