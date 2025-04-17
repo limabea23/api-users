8958d00e867ab3a7bb105c61e6da528915375d53
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-04-17 18:12:37 -0300

    refactor: update server.js

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const usersRoutes = require("./src/routes/usersRoutes");
-const postsRoutes = require("./src/routes/postsRoutes");
+//const postsRoutes = require("./src/routes/postsRoutes");
 
 const app = express();
-const PORT = 5080;
-
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", usersRoutes);
-app.use("/api/posts", postsRoutes);
+app.use("/api", usersRoutes);
+//app.use("/api", postsRoutes);
 
-app.get("/", (req, res) => {
-    res.send("HARRY STYLES IS THE BEST 💗🌟🤠💋");
-});
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Suuucessoooo, servidor rodando na porta ${PORT} 💗🌟🤠💋`);
 });
