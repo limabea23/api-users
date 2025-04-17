7beb5b6b5072861020370f012f50287f26566d6c
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-04-17 18:13:31 -0300

    feat: post model

---
+++ src/models/postModel.js
@@ -0,0 +1,39 @@
+const pool = require("../config/database");
+
+const getAllPosts = async () => {
+    const result = await pool.query("SELECT * FROM posts");
+    return result.rows;
+};
+
+const getPostById = async (id) => {
+    const result = await pool.query("SELECT * FROM posts WHERE id = $1", [id]);
+    return result.rows[0];
+};
+
+const createPost = async (id_post, id_user, conteudo_post, anexo, data_publicacao) => {
+    const result = await pool.query(
+        "INSERT INTO posts (id_post, id_user, conteudo_post, anexo, data_publicacao) VALUES ($1, $2) RETURNING *",
+        [id_post, id_user, conteudo_post, anexo, data_publicacao]
+    );
+    return result.rows[0];
+};
+
+const updatePost = async (id, id_post, id_user, conteudo_post, anexo, data_publicacao) => {
+    const result = await pool.query(
+        "UPDATE posts SET id_post = $1, id_user = $2 conteudo_post = $3 anexo = $4 data_publicacao = $5 WHERE id = $6 RETURNING *",
+        [id_post, id_user, conteudo_post, anexo, data_publicacao, id]
+    );
+    return result.rows[0];
+};
+
+const deletePost = async (id) => {
+    const result = await pool.query("DELETE FROM posts WHERE id = $1 RETURNING *", [id]);
+
+    if (result.rowCount === 0) {
+        return { error: "Post não encontrado." };
+    }
+
+    return { message: "Post deletado com sucesso." };
+};
+
+module.exports = { getAllPosts, getPostById, createPost, updatePost, deletePost };
