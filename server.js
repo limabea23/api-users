0796a109e944dabf013e3fb42e0bed2b9012f6fa
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-04-18 19:56:13 -0300

    refactor: update server.js

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const usersRoutes = require("./src/routes/usersRoutes");
-//const postsRoutes = require("./src/routes/postsRoutes");
+const postsRoutes = require("./src/routes/postsRoutes");
+const setupSwagger = require("./src/config/swagger");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+setupSwagger(app);
 
 app.use("/api", usersRoutes);
-//app.use("/api", postsRoutes);
+app.use("/api", postsRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
