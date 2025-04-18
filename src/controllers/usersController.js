c925989c2b80c9a6eabc3b7b8a9b41e25a0019db
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-04-18 20:25:05 -0300

    refactor: update controller

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -24,14 +24,12 @@ const getUser = async (req, res) => {
 const createUser = async (req, res) => {
     try {
         const { name, username, email } = req.body;
-        const newUser = await userModel.createUser(name, username, email);
+        const photo = req.file ? req.file.filename : null;
+        const newUser = await userModel.createUser(name, username, email, photo);
         res.status(201).json(newUser);
     } catch (error) {
 	console.log(error);
-        if (error.code === "23505") { // Código de erro do PostgreSQL para chave única violada
-            return res.status(400).json({ message: "Usuário já cadastrado." });
-        }
-        res.status(500).json({ message: "Erro ao criar usuário." });
+            json({ message: "Erro ao criar usuário." });
     }
 };
 
